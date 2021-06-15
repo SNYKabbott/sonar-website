4966a759c87092d7d35df3f1351b421ee25116be
Jeriah Bowers <jeriah97bowers@yahoo.com>
2021-06-15 14:13:16 -0400

    changing styling in contact-page header section

--- src/components/pages/contact-page/header/header.jsx
+++ src/components/pages/contact-page/header/header.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './header.module.scss';
 
-const header = () => {
+const Header = () => {
+    const [count, setCount] = useState(0);
+
     return (
         <div className={styles.header}>
             <div className={styles.header__image}></div> {/*-- .header__image end --*/}
             <div className={styles.header__info}>
                 <div className={styles.line}></div> {/*-- .line end --*/}
                 <h1>Contact Me</h1>
+                <button onClick={() => setCount(count - 1)}>Subtract</button>
+                <h1>{count}</h1>
+                <button onClick={() => setCount(count + 1)}>Add</button>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel tortor facilisis, volutpat nulla placerat, tincidunt mi. Nullam vel orci dui.</p>
                 <a href="placeholder" className={styles.header__info__button}>Contact Me</a>
             </div> {/*-- .header__info end --*/}
@@ -15,4 +20,4 @@ const header = () => {
     );
 };
 
-export default header;
+export default Header;
